7eabd2277b59125253025926f43a302ea6ca53e2
Martin Guindon <14781590+MartinGuindon@users.noreply.github.com>
2021-01-26 20:50:24 -0500

    Remove duplicate columns related to customer aggregates

--- models/intermediate/shopify__customers__order_aggregates.sql
+++ models/intermediate/shopify__customers__order_aggregates.sql
@@ -9,8 +9,6 @@ with orders as (
         customer_id,
         min(created_timestamp) as first_order_timestamp,
         max(created_timestamp) as most_recent_order_timestamp,
-        count(*) as number_of_orders,
-        sum(total_price) as lifetime_total_price,
         avg(total_price)  as average_order_value
     from orders
     group by 1
--- models/shopify__customers.sql
+++ models/shopify__customers.sql
@@ -14,9 +14,7 @@ with customers as (
         customers.*,
         orders.first_order_timestamp,
         orders.most_recent_order_timestamp,
-        orders.average_order_value,
-        coalesce(orders.number_of_orders, 0) as number_of_orders,
-        coalesce(orders.lifetime_total_price, 0) as lifetime_total_price
+        orders.average_order_value
     from customers
     left join orders
         using (customer_id)
