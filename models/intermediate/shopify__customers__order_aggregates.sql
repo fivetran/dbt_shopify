f7f4929d74678821c41ac227d6df0a8da953ffbc
Dylan Baker <dylan@dbanalytics.co>
2021-05-28 12:10:51 -0400

    unioned data

--- models/intermediate/shopify__customers__order_aggregates.sql
+++ models/intermediate/shopify__customers__order_aggregates.sql
@@ -24,6 +24,7 @@ with orders as (
 
     select
         orders.customer_id,
+        orders.source_relation,
         min(orders.created_timestamp) as first_order_timestamp,
         max(orders.created_timestamp) as most_recent_order_timestamp,
         avg(case when lower(transactions.kind) in ('sale','capture') then transactions.currency_exchange_calculated_amount end) as average_order_value,
@@ -32,9 +33,9 @@ with orders as (
         count(distinct orders.order_id) as lifetime_count_orders
     from orders
     left join transactions
-        using (order_id)
+        using (order_id, source_relation)
     where customer_id is not null
-    group by 1
+    group by 1,2
 
 )
 
