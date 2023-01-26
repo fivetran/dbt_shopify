77b9094260d394853bab55be321ea0452bf3f007
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-01-26 17:57:51 -0600

    update products

--- models/intermediate/int_shopify__product__order_line_aggregates.sql
+++ models/intermediate/int_shopify__product__order_line_aggregates.sql
@@ -8,22 +8,6 @@ with order_lines as (
     select *
     from {{ ref('shopify__orders')}}
 
-), product_order_aggregated as (
-    select 
-        orders.order_id,
-        orders.source_relation,
-        order_lines.product_id,
-        sum(order_lines.quantity) as order_product_quanity,
-        sum(order_lines.total_discount) as order_product_discount,
-        sum(order_lines.order_line_tax) as order_product_tax 
-    
-    from orders
-    left join order_lines
-        on order_lines.order_id = orders.order_id
-        and order_lines.source_relation = orders.source_relation
-
-    group by 1,2,3
-
 ), product_aggregated as (
     select 
         order_lines.product_id,
@@ -34,26 +18,20 @@ with order_lines as (
         sum(order_lines.pre_tax_price) as subtotal_sold,
         sum(order_lines.quantity_net_refunds) as quantity_sold_net_refunds,
         sum(order_lines.subtotal_net_refunds) as subtotal_sold_net_refunds,
-        {# min(orders.created_timestamp) as first_order_timestamp,
-        max(orders.created_timestamp) as most_recent_order_timestamp, #}
-        null as first_order_timestamp,
-        null as most_recent_order_timestamp,
+        min(orders.created_timestamp) as first_order_timestamp,
+        max(orders.created_timestamp) as most_recent_order_timestamp,
 
         -- new columns
         sum(order_lines.total_discount) as product_total_discount,
         sum(order_lines.order_line_tax) as product_total_tax,
-        avg(product_order_aggregated.order_product_quanity) as average_quantity_per_order,
-        avg(product_order_aggregated.order_product_discount) as product_average_discount_per_order,
-        avg(product_order_aggregated.order_product_tax) as product_average_tax_per_order
+        avg(order_lines.quantity) as average_quantity_per_order_line,
+        avg(order_lines.total_discount) as product_average_discount_per_order_line,
+        avg(order_lines.order_line_tax) as product_average_tax_per_order_line
 
     from order_lines
-    {# left join orders
+    left join orders
         on order_lines.order_id = orders.order_id
-        and order_lines.source_relation = orders.source_relation #}
-    left join product_order_aggregated
-        on order_lines.product_id = product_order_aggregated.product_id
-        and order_lines.source_relation = product_order_aggregated.source_relation
-
+        and order_lines.source_relation = orders.source_relation
     group by 1,2
 
 )
--- models/shopify__products.sql
+++ models/shopify__products.sql
@@ -3,7 +3,7 @@ with products as (
     select *
     from {{ ref('int_shopify__products_with_aggregates') }}
 
-), order_lines as (
+), product_order_lines as (
 
     select *
     from {{ ref('int_shopify__product__order_line_aggregates')}}
@@ -12,23 +12,23 @@ with products as (
 
     select
         products.*,
-        coalesce(order_lines.quantity_sold,0) as total_quantity_sold,
-        coalesce(order_lines.subtotal_sold,0) as subtotal_sold,
-        coalesce(order_lines.quantity_sold_net_refunds,0) as quantity_sold_net_refunds,
-        coalesce(order_lines.subtotal_sold_net_refunds,0) as subtotal_sold_net_refunds,
-        order_lines.first_order_timestamp,
-        order_lines.most_recent_order_timestamp,
+        coalesce(product_order_lines.quantity_sold,0) as total_quantity_sold,
+        coalesce(product_order_lines.subtotal_sold,0) as subtotal_sold,
+        coalesce(product_order_lines.quantity_sold_net_refunds,0) as quantity_sold_net_refunds,
+        coalesce(product_order_lines.subtotal_sold_net_refunds,0) as subtotal_sold_net_refunds,
+        product_order_lines.first_order_timestamp,
+        product_order_lines.most_recent_order_timestamp,
         -- start new columns
-        coalesce(order_lines.average_quantity_per_order,0) as average_quantity_per_order,
-        coalesce(order_lines.product_total_discount,0) as product_total_discount,
-        coalesce(order_lines.product_average_discount_per_order,0) as product_average_discount_per_order,
-        coalesce(order_lines.product_total_tax,0) as product_total_tax,
-        coalesce(order_lines.product_average_tax_per_order,0) as product_average_tax_per_order
+        product_order_lines.average_quantity_per_order_line as average_quantity_per_order_line,
+        coalesce(product_order_lines.product_total_discount,0) as product_total_discount,
+        product_order_lines.product_average_discount_per_order_line as product_average_discount_per_order_line,
+        coalesce(product_order_lines.product_total_tax,0) as product_total_tax,
+        product_order_lines.product_average_tax_per_order_line as product_average_tax_per_order_line
 
     from products
-    left join order_lines
-        on products.product_id = order_lines.product_id
-        and products.source_relation = order_lines.source_relation
+    left join product_order_lines
+        on products.product_id = product_order_lines.product_id
+        and products.source_relation = product_order_lines.source_relation
 )
 
 select *
