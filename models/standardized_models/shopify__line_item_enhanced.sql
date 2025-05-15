23502d16e30162881c0952ba23110edc27184e6f
Avinash Kunnath <avinash.kunnath@fivetran.com>
2025-05-15 10:35:16 -0700

    docs

--- models/standardized_models/shopify__line_item_enhanced.sql
+++ models/standardized_models/shopify__line_item_enhanced.sql
@@ -1,4 +1,4 @@
-{{ config(enabled= var('shopify__standardized_billing_model_enabled', False)) }}
+{{ config(enabled=var('shopify__standardized_billing_model_enabled', True)) }}
 
 with line_items as (
 
