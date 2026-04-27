a870f917ed43866520d21df1160535c6d25f78a1
Avinash Kunnath <avinash.kunnath@fivetran.com>
2026-04-27 12:24:06 -0700

    Schema Change: Field deprecations/sunsetting

--- macros/staging/get_product_variant_columns.sql
+++ macros/staging/get_product_variant_columns.sql
@@ -1,6 +1,5 @@
 {% macro get_product_variant_columns() %}
 
-{# Columns below line 28 to be deprecated. #}
 {% set columns = [
     {"name": "id", "datatype": dbt.type_numeric()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
@@ -16,22 +15,13 @@
     {"name": "compare_at_price", "datatype": dbt.type_float()},
     {"name": "taxable", "datatype": dbt.type_boolean()},
     {"name": "barcode", "datatype": dbt.type_string()},
-    {"name": "old_inventory_quantity", "datatype": dbt.type_numeric()},
     {"name": "inventory_quantity", "datatype": dbt.type_numeric()},
     {"name": "tax_code", "datatype": dbt.type_string()},
     {"name": "available_for_sale", "datatype": dbt.type_boolean()},
     {"name": "display_name", "datatype": dbt.type_string()},
     {"name": "legacy_resource_id", "datatype": dbt.type_bigint()},
     {"name": "requires_components", "datatype": dbt.type_boolean()},
-    {"name": "sellable_online_quantity", "datatype": dbt.type_int()},
-    {"name": "fulfillment_service", "datatype": dbt.type_string()},
-    {"name": "grams", "datatype": dbt.type_float()},
-    {"name": "inventory_management", "datatype": dbt.type_string()},
-    {"name": "weight", "datatype": dbt.type_float()},
-    {"name": "weight_unit", "datatype": dbt.type_string()},
-    {"name": "option_1", "datatype": dbt.type_string()},
-    {"name": "option_2", "datatype": dbt.type_string()},
-    {"name": "option_3", "datatype": dbt.type_string()}
+    {"name": "sellable_online_quantity", "datatype": dbt.type_int()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('product_variant_pass_through_columns')) }}
