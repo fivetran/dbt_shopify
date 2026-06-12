f3a2b0dd44c3423c2d4a6f119ff43b4901845e9e
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2026-06-12 17:15:43 -0500

    Apply suggestions from code review
    Co-authored-by: Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>

--- models/graphql/staging/tmp/stg_shopify_gql__order_line_refund_tmp.sql
+++ models/graphql/staging/tmp/stg_shopify_gql__order_line_refund_tmp.sql
@@ -4,7 +4,7 @@
 
 {{
     shopify.shopify_union_data(
-        table_identifier='refund_line_item', 
+        table_identifier='refund_line_item' if var('shopify_gql_using_refund_line_item', shopify.does_table_exist('refund_line_item', 'shopify_graphql')) else 'order_line_refund'
         database_variable='shopify_database', 
         schema_variable='shopify_schema', 
         default_database=target.database,
