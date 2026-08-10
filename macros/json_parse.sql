15e60fdd8e837925bc711bfcae748f83dc80237e
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2026-08-10 15:14:08 -0400

    use fivetran utils branch

--- macros/json_parse.sql
+++
@@ -1,7 +0,0 @@
-{#
-    DuckDB-compatible override for fivetran_utils.json_parse.
-    DuckDB uses json_extract_string with a JSONPath string rather than variadic key arguments.
-#}
-{% macro duckdb__json_parse(string, string_path) %}
-  json_extract_string({{string}}, '${%- for s in string_path -%}{% if s is number %}[{{ s }}]{% else %}.{{ s }}{% endif %}{%- endfor -%}')
-{% endmacro %}
